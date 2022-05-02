3bd82325494c3fc39d62d5126549f9ee95e4f0d9
Vinicius <vinitrech@gmail.com>
2022-05-02 16:21:28 -0300

    auth for admin user

--- routes/router.go
+++ routes/router.go
@@ -32,20 +32,20 @@ func ConfigRoutes(router *gin.Engine) *gin.Engine {
 		api.POST("login", controllers.Login)
 		api.POST("cadastro", controllers.Cadastro)
 		api.POST("auth/google", controllers.GoogleLogin)
+		api.GET("/getData", controllers.GetData)
 
 		dashboard := api.Group("dashboard", middlewares.Auth())
 		{
 			dashboard.GET("/", controllers.Dashboard)
 		}
 
-		usuarios := api.Group("usuarios", middlewares.Auth())
+		usuarios := api.Group("usuarios", middlewares.AuthAdmin())
 		{
 			usuarios.GET("/", controllers.ShowUsuarios)
 			usuarios.POST("/", controllers.CreateUsuario)
 			usuarios.GET("/:id", controllers.ShowUsuario)
 			usuarios.PUT("/:id", controllers.UpdateUsuario)
 			usuarios.DELETE("/:id", controllers.DeleteUsuario)
-			usuarios.GET("/getData", controllers.GetData)
 		}
 	}
 
--- server/middlewares/auth.go
+++ server/middlewares/auth.go
@@ -1,6 +1,8 @@
 package middlewares
 
 import (
+	"tfweblog/database"
+	"tfweblog/models"
 	"tfweblog/services"
 
 	"github.com/gin-gonic/gin"
@@ -19,6 +21,50 @@ func Auth() gin.HandlerFunc {
 		token := header[len(Bearer_schema):]
 
 		if !services.NewJWTService().ValidateToken(token) {
+			c.AbortWithStatus(401)
+		}
+	}
+}
+
+func AuthAdmin() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		const Bearer_schema = "Bearer "
+
+		header := c.GetHeader("Authorization")
+
+		if header == "" {
+			c.AbortWithStatus(401)
+		}
+
+		token := header[len(Bearer_schema):]
+
+		if !services.NewJWTService().ValidateToken(token) {
+			c.AbortWithStatus(401)
+		}
+
+		userId, err := services.NewJWTService().GetIDFromToken(token)
+
+		if err != nil {
+			c.JSON(500, gin.H{
+				"error": "Não foi possível decodificar o token: " + err.Error(),
+			})
+			return
+		}
+
+		db := database.GetDatabase()
+
+		var usuario models.Usuario
+
+		err = db.Select([]string{"id", "nome", "email", "cargo"}).Where("id = ?", userId).Find(&usuario).Error
+
+		if err != nil {
+			c.JSON(400, gin.H{
+				"error": "Registro não encontrado: " + err.Error(),
+			})
+			return
+		}
+
+		if usuario.Cargo != "administrador" {
 			c.AbortWithStatus(401)
 		}
 	}
