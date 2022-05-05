d91e7f39618d3f463f0b56e9bd90b505ba53a7fb
Vinicius <vinitrech@gmail.com>
2022-05-05 14:22:30 -0300

    consulta cidades pronta para async slect

--- controllers/cidadeController.go
+++ controllers/cidadeController.go
@@ -14,13 +14,27 @@ func ShowCidades(c *gin.Context) {
 		Nome string
 	}
 
-	err := db.Raw("select cidades.id, CONCAT(cidades.nome, ' - ', estados.sigla) as nome from cidades join estados on estados.id = cidades.id_estado order by estados.sigla, cidades.nome").Scan(&cidades).Error
+	search, hasSearch := c.GetQuery("search")
+	search = "%" + search + "%"
 
-	if err != nil {
-		c.JSON(400, gin.H{
-			"error": "Não foi possível listar os registros. " + err.Error(),
-		})
-		return
+	if hasSearch {
+		err := db.Raw("select cidades.id, CONCAT(cidades.nome, ' - ', estados.sigla) as nome from cidades join estados on estados.id = cidades.id_estado where cidades.nome ILIKE ? order by estados.sigla, cidades.nome limit 10", search).Scan(&cidades).Error
+
+		if err != nil {
+			c.JSON(400, gin.H{
+				"error": "Não foi possível listar os registros. " + err.Error(),
+			})
+			return
+		}
+	} else {
+		err := db.Raw("select cidades.id, CONCAT(cidades.nome, ' - ', estados.sigla) as nome from cidades join estados on estados.id = cidades.id_estado order by estados.sigla, cidades.nome limit 10").Scan(&cidades).Error
+
+		if err != nil {
+			c.JSON(400, gin.H{
+				"error": "Não foi possível listar os registros.",
+			})
+			return
+		}
 	}
 
 	c.JSON(200, cidades)
