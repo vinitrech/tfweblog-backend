a842b77088cf31c70f4e558c618ca4f9cc7a2e8a
Vinicius <vinitrech@gmail.com>
2022-05-10 11:46:55 -0300

    teste

--- routes/router.go
+++ routes/router.go
@@ -4,13 +4,28 @@ import (
 	"tfweblog/controllers"
 	"tfweblog/server/middlewares"
 
-	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
+}
+
 func ConfigRoutes(router *gin.Engine) *gin.Engine {
 
-	router.Use(cors.Default())
+	router.Use(CORSMiddleware())
 
 	api := router.Group("api/v1")
 	{
