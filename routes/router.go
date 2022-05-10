a7ec395deed7a88657d3773c15eac44e92560037
Vinicius <vinitrech@gmail.com>
2022-05-10 14:57:03 -0300

    cors

--- routes/router.go
+++ routes/router.go
@@ -7,25 +7,7 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func CORSMiddleware() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "https://tfweblog-frontend.herokuapp.com")
-		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
-		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
-		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
-
-		if c.Request.Method == "OPTIONS" {
-			c.AbortWithStatus(204)
-			return
-		}
-
-		c.Next()
-	}
-}
-
 func ConfigRoutes(router *gin.Engine) *gin.Engine {
-
-	router.Use(CORSMiddleware())
 
 	api := router.Group("api/v1")
 	{
