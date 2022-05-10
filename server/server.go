e4882e45f5e0619d50222f2867aaf425384c1e75
Vinicius <vinitrech@gmail.com>
2022-05-10 15:11:38 -0300

    allowing cors

--- server/server.go
+++ server/server.go
@@ -17,25 +17,9 @@ func NewServer() Server {
 	}
 }
 
-
-func CORSMiddleware() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "https://tfweblog-frontend.herokuapp.com")
-		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
-		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
-		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
-
-		if c.Request.Method == "OPTIONS" {
-			c.AbortWithStatus(204)
-			return
-		}
-
-		c.Next()
-	}
-}
-
 func (s *Server) Run() {
+
 	router := routes.ConfigRoutes(s.server)
-	router.Use(CORSMiddleware())
+
 	log.Fatal(router.Run())
 }
